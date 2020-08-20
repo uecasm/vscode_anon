dbf90168e2f0d06ad55bdd246e46365ae518a2da
Gavin Lambert <uecasm@users.noreply.github.com>
2020-08-20 19:04:33 +1200

    Initial commit

---
+++ anon.cpp
@@ -0,0 +1,11 @@
+namespace
+{
+    #include "meow.h"
+}
+
+int main()
+{
+    Meow feline;
+
+    return 0;
+}
---
+++ meow.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "meow_fwd.h"
+
+struct _Meow
+{
+    int kitten;
+};
